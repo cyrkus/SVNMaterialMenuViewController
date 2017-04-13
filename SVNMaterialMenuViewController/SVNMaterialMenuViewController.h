abd886d9cfb4301bfdaadc9a863ad227d1e7767b
Aaron Bikis <aaron.bikis@gmail.com>
2017-04-13 16:08:55 -0700

    Initial Commit

---
+++ SVNMaterialMenuViewController/SVNMaterialMenuViewController.h
@@ -0,0 +1,19 @@
+//
+//  SVNMaterialMenuViewController.h
+//  SVNMaterialMenuViewController
+//
+//  Created by Aaron Dean Bikis on 4/13/17.
+//  Copyright © 2017 7apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVNMaterialMenuViewController.
+FOUNDATION_EXPORT double SVNMaterialMenuViewControllerVersionNumber;
+
+//! Project version string for SVNMaterialMenuViewController.
+FOUNDATION_EXPORT const unsigned char SVNMaterialMenuViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVNMaterialMenuViewController/PublicHeader.h>
+
+
